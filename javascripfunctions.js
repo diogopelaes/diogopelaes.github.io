4c2c574e9890076df8f389a20b6ad06438fc4fae
Diogo <58195309+diogopelaes@users.noreply.github.com>
2022-05-15 17:39:11 -0300

    font

--- javascripfunctions.js
+++ javascripfunctions.js
@@ -1,5 +1,27 @@
 var elem = document.documentElement;
 
+function aumentar(){
+    var hora = document.getElementById('hora');
+    var style_hora = window.getComputedStyle(hora, null).getPropertyValue('font-size');
+    var fontSize_hora = parseFloat(style_hora); 
+    hora.style.fontSize = (fontSize_hora + 5) + 'px';
+    var hoje = document.getElementById('hoje');
+    var style_hoje = window.getComputedStyle(hoje, null).getPropertyValue('font-size');
+    var fontSize_hoje = parseFloat(style_hoje); 
+    hoje.style.fontSize = (fontSize_hoje + 5) + 'px';
+}
+
+function diminuir(){
+    var hora = document.getElementById('hora');
+    var style_hora = window.getComputedStyle(hora, null).getPropertyValue('font-size');
+    var fontSize_hora = parseFloat(style_hora); 
+    hora.style.fontSize = (fontSize_hora - 5) + 'px';
+    var hoje = document.getElementById('hoje');
+    var style_hoje = window.getComputedStyle(hoje, null).getPropertyValue('font-size');
+    var fontSize_hoje = parseFloat(style_hoje); 
+    hoje.style.fontSize = (fontSize_hoje - 5) + 'px';
+}
+
 function openFullscreen() {
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
