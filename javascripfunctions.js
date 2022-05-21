98dc77493b50d4c9016729978ad7231b7ab0b885
Diogo <58195309+diogopelaes@users.noreply.github.com>
2022-05-21 18:10:28 -0300

    frac

--- javascripfunctions.js
+++ javascripfunctions.js
@@ -59,31 +59,56 @@ if (mes < 10) {
 document.getElementById('hoje').textContent = dia + "/" + mes + "/" + today.getFullYear();
 }
 
-window.onload = function horario() {
-    var today = new Date();
-    var hora = today.getHours();
-    if (hora < 10) {
-        hora = '0' + hora;
+function fraction(esse, n) {
+    document.getElementById('denominador'+n).innerHTML = esse.value;
+    var denominador = parseInt(esse.value);
+    let table_column  = document.getElementById('parte'+n).cloneNode(true);
+    table_column.className='partes';
+    table_column.setAttribute('name', '');
+    let element = document.getElementById('fig'+n);
+    while (element.childElementCount - denominador >= 0) {
+        element.removeChild(element.lastChild);
     }
-    var min = today.getMinutes();
-    if (min < 10) {
-        min = '0' + min;
+    while (element.childElementCount - denominador < 0) {
+        element.appendChild(table_column.cloneNode(true));
     }
-    var sec = today.getSeconds();
-    if (sec < 10) {
-        sec = '0' + sec;
+    numerador('numerador'+n);
+}
+
+function pintar_parte(esse, n) {
+    if (esse.classList.contains('parte_pintada')){
+        esse.className='partes';
+        esse.setAttribute('name', '');
+    }else{
+        esse.classList.add('parte_pintada');
+        esse.setAttribute('name', 'numerador'+n);
     }
-    document.getElementById('hora').textContent = hora + ":" + min + ":" + sec;
-    if (hora == 0 || document.getElementById('hoje').textContent == "") {
-        var dia = today.getDate();
-        if (dia < 10) {
-            dia = '0' + dia;
-        }
-        var mes = today.getMonth() + 1;
-        if (mes < 10) {
-            mes = '0' + mes;
-        }
-        document.getElementById('hoje').textContent = dia + "/" + mes + "/" + today.getFullYear();
+    numerador('numerador'+n);
+}
+
+function numerador(idNumerador){
+    document.getElementById('verificar').hidden = false;
+    document.getElementById('resul').hidden = true;
+    document.getElementById('resul_text').innerHTML = '';
+    document.getElementById(idNumerador).innerHTML = document.getElementsByName(idNumerador).length;
+}
+
+function verificar(){
+    document.getElementById('verificar').hidden = true;
+    document.getElementById('resul').hidden = false;
+    document.getElementById('n_resul1').innerHTML = document.getElementById('numerador1').innerHTML;
+    document.getElementById('n_resul2').innerHTML = document.getElementById('numerador2').innerHTML;
+    document.getElementById('d_resul1').innerHTML = document.getElementById('denominador1').innerHTML;
+    document.getElementById('d_resul2').innerHTML = document.getElementById('denominador2').innerHTML;
+    var n1 = parseInt(document.getElementById('numerador1').innerHTML);
+    var d1 = parseInt(document.getElementById('denominador1').innerHTML);
+    var n2 = parseInt(document.getElementById('numerador2').innerHTML);
+    var d2 = parseInt(document.getElementById('denominador2').innerHTML);
+    if (n1/d1 == n2/d2){
+        document.getElementById('igual').innerHTML = '=';
+        document.getElementById('resul_text').innerHTML = 'São frações equivalentes';
+    }else{
+        document.getElementById('igual').innerHTML = '≠';
+        document.getElementById('resul_text').innerHTML = 'NÃO são frações equivalentes';
     }
-    const myTimeout = setTimeout(horario, 100);
 }
