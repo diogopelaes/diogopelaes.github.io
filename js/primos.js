e82757641f85a491a5e865eac879c1e3e24f4631
Diogo <58195309+diogopelaes@users.noreply.github.com>
2022-05-22 16:55:17 -0300

    atualizar2

---
+++ js/primos.js
@@ -0,0 +1,8 @@
+window.onload = function carregar() {
+    let 
+    for (i = 0; i < 5; i++){
+        for (j = 0; j < 10; j++){
+
+        }
+    }
+}
--- js/relogio.js
+++ js/relogio.js
@@ -1,12 +1,41 @@
 function data_hoje() {
-var today = new Date();
-var dia = today.getDate();
-if (dia < 10) {
-    dia = '0' + dia;
+    var today = new Date();
+    var dia = today.getDate();
+    if (dia < 10) {
+        dia = '0' + dia;
+    }
+    var mes = today.getMonth() + 1;
+    if (mes < 10) {
+        mes = '0' + mes;
+    }
+    document.getElementById('hoje').textContent = dia + "/" + mes + "/" + today.getFullYear();
 }
-var mes = today.getMonth() + 1;
-if (mes < 10) {
-    mes = '0' + mes;
-}
-document.getElementById('hoje').textContent = dia + "/" + mes + "/" + today.getFullYear();
+
+window.onload = function horario() {
+    var today = new Date();
+    var hora = today.getHours();
+    if (hora < 10) {
+        hora = '0' + hora;
+    }
+    var min = today.getMinutes();
+    if (min < 10) {
+        min = '0' + min;
+    }
+    var sec = today.getSeconds();
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    document.getElementById('hora').textContent = hora + ":" + min + ":" + sec;
+    if (hora == 0 || document.getElementById('hoje').textContent == "") {
+        var dia = today.getDate();
+        if (dia < 10) {
+            dia = '0' + dia;
+        }
+        var mes = today.getMonth() + 1;
+        if (mes < 10) {
+            mes = '0' + mes;
+        }
+        document.getElementById('hoje').textContent = dia + "/" + mes + "/" + today.getFullYear();
+    }
+    const myTimeout = setTimeout(horario, 100);
 }
